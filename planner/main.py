5ff3849826d7bd62c33b63515f73916563af81ae
hyunwoo4073 <qogusdn13@gmail.com>
2026-01-07 14:12:54 +0900

    SQL 데이터베이스 연동 및 CRUD 처리 구현

--- planner/main.py
+++ planner/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn
 
 import uvicorn
 
@@ -8,6 +9,14 @@ app = FastAPI()
 # 라우트 등록
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+@app.on_event("startup")
+def on_startup():
+    conn()
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- planner/models/events.py
+++ planner/models/events.py
@@ -1,13 +1,33 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import List, Optional
 
-class Event(BaseModel):
-    id: int # 자동 생성되는 고유 식별자
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True) # 자동 생성되는 고유 식별자
     title: str # 이벤트 타이틀
     image: str # 이벤트 이미지 배너의 링크
     description: str # 이벤트 설명
-    tags: List[str] # 그룹화를 위한 이벤트 태그
+    tags: List[str] = Field(sa_column=Column(JSON))# 그룹화를 위한 이벤트 태그
     location: str # 이벤트 위치
+
+    class Config:
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event. \
+                Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
+
+class EventUpdate(SQLModel):
+    title: Optional[str] = None
+    image: Optional[str] = None
+    description: Optional[str] = None
+    tags: Optional[List[str]] = None
+    location: Optional[str] = None
 
     class Config:
         schema_extra = {
